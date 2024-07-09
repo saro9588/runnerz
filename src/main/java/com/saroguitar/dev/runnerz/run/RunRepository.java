06b12af5eb3f1529b9d90294bb2605331aa5270c
Saro <babikiansaro@gmail.com>
2024-07-09 14:48:47 -0700

    set-up-api-and-http-methods

---
+++ src/main/java/com/saroguitar/dev/runnerz/run/RunRepository.java
@@ -0,0 +1,51 @@
+package com.saroguitar.dev.runnerz.run;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Repository
+public class RunRepository {
+
+    private List<Run> runs = new ArrayList<>();
+    List<Run> findAll() {
+        return runs;
+    }
+
+    Optional<Run> findById(Integer id) {
+        return runs.stream()
+                .filter(run -> Objects.equals(run.id(), id))
+                .findFirst();
+    }
+
+    void create(Run run) {
+        runs.add(run);
+    }
+
+    @PostConstruct
+    private void init() {
+        runs.add(new Run(
+                1,
+                "Monday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(60),
+                6,
+                Location.INDOOR
+        ));
+
+        runs.add(new Run(
+                2,
+                "Wednesday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(60),
+                6,
+                Location.INDOOR
+        ));
+    }
+}
