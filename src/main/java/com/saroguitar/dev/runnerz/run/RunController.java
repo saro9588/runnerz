85cdf19bfdcffd50fdb6a6bd52d82a70277f03f6
Saro <babikiansaro@gmail.com>
2024-07-09 21:56:18 -0700

    add-validation-to-method-calls

--- src/main/java/com/saroguitar/dev/runnerz/run/RunController.java
+++ src/main/java/com/saroguitar/dev/runnerz/run/RunController.java
@@ -1,5 +1,6 @@
 package com.saroguitar.dev.runnerz.run;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -35,14 +36,14 @@ public class RunController {
     //post
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    void create(@RequestBody Run run) {
+    void create(@Valid @RequestBody Run run) {
         runRepository.create(run);
     }
 
     //put
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping ("/{id}")
-    void update(@RequestBody Run run, @PathVariable Integer id) {
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
         runRepository.update(run, id);
     }
 
