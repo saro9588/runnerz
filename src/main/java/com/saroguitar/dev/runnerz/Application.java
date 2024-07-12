161956558aae4d7e3025ed23ce4be2a1496889da
Saro <babikiansaro@gmail.com>
2024-07-11 17:48:44 -0700

    utilizing-spring-HttpClient-to-make-calls-to-a-services-no-need-to-write-method-calls

--- src/main/java/com/saroguitar/dev/runnerz/Application.java
+++ src/main/java/com/saroguitar/dev/runnerz/Application.java
@@ -1,6 +1,7 @@
 package com.saroguitar.dev.runnerz;
 
 import com.saroguitar.dev.runnerz.user.User;
+import com.saroguitar.dev.runnerz.user.UserHttpClient;
 import com.saroguitar.dev.runnerz.user.UserRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.util.List;
 
@@ -19,6 +24,24 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		log.info("Application started!");
+	}
+
+	@Bean
+	UserHttpClient userHttpClient(){
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(UserHttpClient.class);
+	}
+
+	@Bean
+	CommandLineRunner runner1(UserHttpClient client) {
+		return args -> {
+			List<User> users = client.findAll();
+			System.out.println(users);
+
+			User user = client.findById(1);
+			System.out.println(user);
+		};
 	}
 
 	@Bean
